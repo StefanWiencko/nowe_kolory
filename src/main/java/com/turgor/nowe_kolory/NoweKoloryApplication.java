f359c82557fe778ffb3d11fd25c06327d1d3a216
StefanWiencko <stefanwiencko2@gmail.com>
2024-01-05 16:11:57 +0100

    first commit

---
+++ src/main/java/com/turgor/nowe_kolory/NoweKoloryApplication.java
@@ -0,0 +1,13 @@
+package com.turgor.nowe_kolory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoweKoloryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoweKoloryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turgor/nowe_kolory/NoweKoloryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turgor.nowe_kolory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoweKoloryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
